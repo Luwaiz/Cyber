4fa99b8f96915334f4cdd98fe2daee5d30259c7c
Eluwa Emmanuel <emmaeluwa2021@gmail.com>
2024-07-09 18:56:28 +0100

    first commit

--- screens/AuthScreen/signup/styles.js
+++ screens/AuthScreen/signup/styles.js
@@ -14,7 +14,6 @@ export const styles = StyleSheet.create({
         marginTop: 20,
         marginBottom: 20,
         marginHorizontal:30,
-
     },
     subTitle: {
         fontWeight: 'regular',
